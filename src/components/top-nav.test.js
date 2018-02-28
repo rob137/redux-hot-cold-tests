5e2c96d7c09c1cec708f750c36ddd8086cd55f45
Robert Kirby <robertaxelkirby@gmail.com>
2018-02-28 17:14:34 +0000

    initial Enzyme tests

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,30 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import TopNav from './top-nav';
+import { TopNav } from './top-nav';
+import { RESTART_GAME, GENERATE_AURAL_UPDATE } from '../actions';
 
 describe('<TopNav />', () => {
-  it('Renders without crashing', () => {
+  it('renders without crashing', () => {
     shallow(<TopNav />);
   });
 
-  it('Should call onNewGame when new game is clicked', () => {
-    const callback = jest.fn();
-    const wrapper = shallow(<TopNav onRestartGame={callback} />);
+  it('dispatches restartGame on click to \'new game\'', () => {
+    const dispatch = jest.fn();
+    const wrapper = shallow(<TopNav dispatch={dispatch} />);
     const link = wrapper.find('.new');
-    link.simulate('click', {
-      preventDefault() { }
-    });
-    expect(callback).toHaveBeenCalled();
-  });
-
-  it('Should call onGenerateAuralUpdate when state-of-game link is clicked', () => {
-    const callback = jest.fn();
-    const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
-    const link = wrapper.find('.status-link');
-    link.simulate('click', {
-      preventDefault() { }
-    });
-    expect(callback).toHaveBeenCalled();
-  });
+    link.simulate('click');
+    expect(dispatch).toHaveBeenCalled();
+  })
 });
